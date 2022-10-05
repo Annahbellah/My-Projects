114b4bf2b4860557ca9b6bc0585c9352726c9b8e
Anne Agbo <agboanneananwune@gmail.com>
2022-10-06 00:22:28 +0100

    week 2 task done

--- Bella-store/src/test/java/dev/decagon/model/CashierTest.java
+++ Bella-store/src/test/java/dev/decagon/model/CashierTest.java
@@ -23,41 +23,32 @@ class CashierTest {
         cart.add(product3);
 
         Customers customer = new Customers("Stephen King", 28, "sk@gmail.com", 12, 50000, cart);
-//        customer.buy(customer, product1);
-//        customer.buy(customer, product2);
-//        customer.buy(customer, product3);
+
 
         Cashier cashier = new Cashier("Baby Girl", 23, "babygirl4live@gmail.com", 31);
 
         String expectedMessage = "Insufficient Fund";
-      //  Assert.assertEquals("SOLD", result1);
+
          Assert.assertThrows(InsufficientFundException.class,()->cashier.sellProducts(product2, customer));
 
 
     }
 
-    @Test
+   @Test
     void issueReceipts() {
         Products product1 = new Products(1L, "Joy Soap", 200, 50, "Bar Soap", "Cosmetics");
-        Products product2 = new Products(2L, "Pears", 5000, 20, "Body Lotion", "Cosmetics");
-        Products product3 = new Products(3L, "Always", 2000, 100,"Sanitary Pad", "toiletories" );
 
         ArrayList<Products> cart = new ArrayList();
         cart.add(product1);
-        cart.add(product2);
-        cart.add(product3);
 
         Customers customer = new Customers("Stephen King", 28, "sk@gmail.com", 12, 50000, cart);
         customer.buy(customer, product1);
-        customer.buy(customer, product2);
-        customer.buy(customer, product3);
+
 
         Cashier cashier = new Cashier("Baby Girl", 23, "babygirl4live@gmail.com", 31);
         String result1 = cashier.issueReceipts(customer, product1);
-//        String result2 = cashier.issueReceipts(customer, product1);
 
-        Assert.assertEquals("RECEIPT", result1);
-//        Assert.assertEquals("Product is out of stock", result2);
+       Assert.assertEquals("RECEIPT", result1);
 
     }
 }
